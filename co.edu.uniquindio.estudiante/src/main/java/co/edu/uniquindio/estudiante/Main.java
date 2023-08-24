62f9f382655ecab6b0292b8f86aac2fe2625379c
JaviLeyton <oscarj.leytono@uqvirtual.edu.co>
2023-08-24 18:56:47 -0500

    :+1:  Primer commit

---
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.estudiante;
+import co.edu.uniquindio.estudiante.model.Estudiante;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/model/Estudiante.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.estudiante.model;
+
+public class Estudiante {
+    public static void main (String[] args)
+    {
+        System.out.println ("Vamos a morir");
+    }
+
+}
